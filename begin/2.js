3ca1f233202cd5f59af22a707610b09f035d94d4
Ermicioi Costel <contrast-design@mail.ru>
2019-03-02 03:21:07 -0800

    begin problem 2

---
+++ begin/2.js
@@ -0,0 +1,8 @@
+/**
+ * Given the side a of a square, find the area S of the square: S = a2
+ */
+
+function area(a) {
+  return a * a;
+}
+module.exports = area;
---
+++ begin/2.test.js
@@ -0,0 +1,5 @@
+const area = require("./2");
+
+test("should return correct result", () => {
+  expect(area(3)).toBe(9);
+});
