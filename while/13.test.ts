98faed388f1175ca89c1fa0925d927453537224f
Costel Ermicioi~ <ermicioi.costel@gmail.com>
2019-06-22 11:29:39 +0300

    while problem 13

---
+++ while/13.test.ts
@@ -0,0 +1,10 @@
+import { F } from "./12";
+test("should return correct results", () => {
+  expect(F(10)).toEqual([4, 10]);
+  expect(F(7)).toEqual([3, 6]);
+  expect(F(8)).toEqual([3, 6]);
+  expect(F(9)).toEqual([3, 6]);
+  expect(F(35)).toEqual([7, 28]);
+  expect(F(13)).toEqual([4, 10]);
+  expect(F(5)).toEqual([2, 3]);
+});
---
+++ while/13.ts
@@ -0,0 +1,14 @@
+/**
+   An integer N (> 1) is given. Find the smallest integer K such 
+   that the sum 1 + 2 + … + K is greater than or equal to N. 
+   Output K and the corresponding sum.
+ */
+export function F(N: number) {
+  let k = 1;
+  let sum = k;
+  while (sum <= N) {
+    k = k + 1;
+    sum = sum + k;
+  }
+  return [k - 1, sum - k];
+}
