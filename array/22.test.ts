29a7f92ce919f5b218bd5fd9d1b36eed79cce1d0
CostelEr <ermicioicostel@gmail.com>
2019-07-13 04:34:30 +0300

    array problem 22

--- array/22.test.ts
+++ array/22.test.ts
@@ -3,4 +3,6 @@ test("should return correct results", () => {
   expect(F(9, [6.5, 6.4, 0.6, 5.1, 7.7, 1.9, 4.3, 9.4, 6.3], 4, 9)).toEqual(
     13.5
   );
+  expect(F(5, [2.3, 5.2, 5.7, 5.3, 4], 5, 5)).toEqual(18.5);
+  expect(F(7, [2.7, 2.5, 6.1, 2.3, 6.5, 4.9, 6.6], 4, 7)).toEqual(11.3);
 });
